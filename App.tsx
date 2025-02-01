b5faef929a5819db6460eb72a102c90a161e3785
devdk12 <devdk2208@gmail.com>
2025-02-01 15:18:43 +0530

    Implement permission requests for camera, microphone, and photo library

--- App.tsx
+++ App.tsx
@@ -1,7 +1,18 @@
+import { useEffect } from "react";
 import "./src/global.css";
 import Navigation from "./src/navigation/Navigation";
+import { requestPermissions } from "./src/utils/Helpers";
 
 const App = () => {
+
+  useEffect(() => {
+    const fetchPermissions = async () => {
+      await requestPermissions();
+    }
+    fetchPermissions();
+  }, [])
+
+
   return (
     <Navigation />
   )
--- src/utils/Helpers.ts
+++ src/utils/Helpers.ts
@@ -1,4 +1,16 @@
-
+import { Platform } from 'react-native';
+import {
+  launchImageLibrary,
+  Asset,
+  ImagePickerResponse
+} from 'react-native-image-picker';
+import {
+  requestMultiple,
+  PERMISSIONS,
+  RESULTS,
+  request,
+  Permission,
+} from 'react-native-permissions';
 
 
 export const calculateFinalPos = (x: number, y: number, containerWidth: number, containerHeight: number) => {
@@ -38,3 +50,107 @@ export const calculateFinalPos = (x: number, y: number, containerWidth: number, 
 
   return { finalX, finalY };
 }
+
+const logPermissionStatus = (permission: string, status: string) => {
+  if (status === RESULTS.GRANTED) {
+    console.log(`${permission} PERMISSION GRANTED ✅`);
+  } else if (status === RESULTS.DENIED) {
+    console.log(`${permission} PERMISSION DENIED ❌`);
+  } else if (status === RESULTS.BLOCKED) {
+    console.log(`${permission} PERMISSION BLOCKED 🚫`);
+  } else {
+    console.log(`${permission} PERMISSION STATUS: ${status}`);
+  }
+};
+
+export const requestPermissions = async () => {
+  try {
+    const permissionsToRequest =
+      Platform.OS === 'ios'
+        ? [
+          PERMISSIONS.IOS.CAMERA,
+          PERMISSIONS.IOS.MICROPHONE,
+          PERMISSIONS.IOS.PHOTO_LIBRARY,
+        ]
+        : [
+          PERMISSIONS.ANDROID.CAMERA,
+          PERMISSIONS.ANDROID.RECORD_AUDIO,
+          PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE,
+        ];
+
+    const results = await requestMultiple(permissionsToRequest);
+
+    for (const [permission, status] of Object.entries(results)) {
+      logPermissionStatus(permission, status);
+    }
+
+    const isCameraGranted =
+      Platform.OS === 'ios'
+        ? results[PERMISSIONS.IOS.CAMERA] === 'granted'
+        : results[PERMISSIONS.ANDROID.CAMERA] === 'granted';
+
+    const isMicrophoneGranted =
+      Platform.OS === 'ios'
+        ? results[PERMISSIONS.IOS.MICROPHONE] === 'granted'
+        : results[PERMISSIONS.ANDROID.RECORD_AUDIO] === 'granted';
+
+    const isPhotoLibraryGranted =
+      Platform.OS === 'ios'
+        ? results[PERMISSIONS.IOS.PHOTO_LIBRARY] === 'granted'
+        : results[PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE] === 'granted';
+
+    return { isCameraGranted, isMicrophoneGranted, isPhotoLibraryGranted };
+  } catch (error) {
+    console.error('Error requesting permissions:', error);
+    return { isCameraGranted: false, isMicrophoneGranted: false };
+  }
+};
+
+
+export const requestPermission = async (permission: string, iosPermission: Permission, androidPermission: Permission) => {
+  try {
+    const permissionToRequest = Platform.OS === 'ios' ? iosPermission : androidPermission;
+
+    const status = await request(permissionToRequest);
+
+    logPermissionStatus(permissionToRequest, status);
+
+    return status === 'granted';
+  } catch (error) {
+    console.error(`Error requesting ${permission.toLowerCase()} permission:`, error);
+    return false;
+  }
+};
+
+export const requestCameraPermission = async () => requestPermission('Camera', PERMISSIONS.IOS.CAMERA, PERMISSIONS.ANDROID.CAMERA);
+
+export const requestPhotoLibraryPermission = async () => requestPermission('Photo Library', PERMISSIONS.IOS.PHOTO_LIBRARY, PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE);
+
+
+
+export const requestMicrophonePermission = async () => requestPermission('Microphone', PERMISSIONS.IOS.MICROPHONE, PERMISSIONS.ANDROID.RECORD_AUDIO);
+
+
+export type TOnMediaPickedUp = (image: Asset) => void;
+export const pickImage = (onMediaPickedUp: TOnMediaPickedUp) => {
+  launchImageLibrary(
+    {
+      mediaType: 'photo',
+      quality: 1,
+      includeBase64: false,
+    },
+    (response: ImagePickerResponse) => {
+      if (response.didCancel) {
+        console.log('User canceled image picker');
+      } else if (response.errorCode) {
+        console.log('ImagePicker Error: ', response.errorMessage);
+      } else {
+        const { assets } = response;
+        if (assets && assets.length > 0) {
+          const selectedImage = assets[0];
+          onMediaPickedUp(selectedImage);
+        }
+      }
+    },
+  );
+};
