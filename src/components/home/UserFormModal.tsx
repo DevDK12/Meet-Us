2e6533b4f12089e682a6f301c2df24194ff49a2d
devdk12 <devdk2208@gmail.com>
2025-02-01 16:05:16 +0530

    Refactor UserFormModal to include KeyboardAvoidingView and ScrollView for better user experience

--- src/components/home/UserFormModal.tsx
+++ src/components/home/UserFormModal.tsx
@@ -1,5 +1,5 @@
 import { FC, useEffect, useState } from 'react'
-import { View, Modal, TextInput, TouchableOpacity, Alert, Image } from 'react-native'
+import { View, Modal, TextInput, TouchableOpacity, Alert, Image, ScrollView, KeyboardAvoidingView, Platform } from 'react-native'
 import CustomText from '../ui/CustomText'
 import { RFValue } from 'react-native-responsive-fontsize'
 import { pickImage } from '../../utils/Helpers'
@@ -64,68 +64,81 @@ const UserFormModal: FC<UserFormModalProps> = ({ isVisible, onClose }) => {
             <View
                 className='flex-1 justify-end bg-black/50'
             >
-                <View className='bg-white p-5 rounded-t-3xl'>
-                    <CustomText
-                        fontSize={18}
-                        fontFamily='OpenSans-Medium'
-                        style={{
-                            marginBottom: 15,
-                            fontWeight: 'bold',
-                        }}
-                    >
-                        Enter your details
-                    </CustomText>
+                <KeyboardAvoidingView
+                    behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+                    className='flex-1'
+                >
 
-                    <TouchableOpacity
-                        onPress={handleImageUpload}
-                        className='w-[100px] h-[100px] self-center rounded-full  border-4 border-[#ccc] mb-4'
+                    <ScrollView
+                        contentContainerClassName='grow justify-end'
                     >
-                        {
-                            profilePhotoUrl && (
-                                <Image
-                                    source={{ uri: profilePhotoUrl }}
-                                    className='w-full h-full rounded-full'
-                                />
-                            )
-                        }
-                        <View className='absolute inset-0 bg-[#cccccc40]  rounded-full items-center justify-center p-2'>
-                            <ImageUp color={'#cccccc90'} size={30} />
+
+                        <View className='bg-white p-5 rounded-t-3xl'>
+                            <CustomText
+                                fontSize={18}
+                                fontFamily='OpenSans-Medium'
+                                style={{
+                                    marginBottom: 15,
+                                    fontWeight: 'bold',
+                                }}
+                            >
+                                Enter your details
+                            </CustomText>
+
+                            <TouchableOpacity
+                                onPress={handleImageUpload}
+                                className='w-[100px] h-[100px] self-center rounded-full  border-4 border-[#ccc] mb-4'
+                            >
+                                {
+                                    profilePhotoUrl && (
+                                        <Image
+                                            source={{ uri: profilePhotoUrl }}
+                                            className='w-full h-full rounded-full'
+                                        />
+                                    )
+                                }
+                                <View className='absolute inset-0 bg-[#cccccc40]  rounded-full items-center justify-center p-2'>
+                                    <ImageUp color={'#cccccc90'} size={30} />
+                                </View>
+                            </TouchableOpacity>
+                            <TextInput
+                                placeholder='Enter your name'
+                                className='h-[50px] border-2 border-[#ccc] rounded-md px-4 mb-[15px]'
+                                style={{ fontFamily: 'OpenSans-Regular', fontSize: RFValue(12) }}
+                                value={name}
+                                onChangeText={setName}
+                                placeholderTextColor={'#ccc'}
+                            />
+
+                            <View className='flex-row justify-between'>
+                                <TouchableOpacity
+                                    className='flex-1 bg-primary py-4 mx-2 rounded-3xl items-center '
+                                    onPress={handleSave}
+                                >
+                                    <CustomText
+                                        fontFamily='OpenSans-Medium'
+                                        style={{ color: 'white', fontWeight: 'bold' }}
+                                    >
+                                        Save
+                                    </CustomText>
+                                </TouchableOpacity>
+                                <TouchableOpacity
+                                    className='flex-1 bg-[#FF5A5F] py-4 mx-2 rounded-3xl items-center '
+                                    onPress={onClose}
+                                >
+                                    <CustomText
+                                        fontFamily='OpenSans-Medium'
+                                        style={{ color: 'white', fontWeight: 'bold' }}
+                                    >
+                                        Cancel
+                                    </CustomText>
+                                </TouchableOpacity>
+                            </View>
                         </View>
-                    </TouchableOpacity>
-                    <TextInput
-                        placeholder='Enter your name'
-                        className='h-[50px] border-2 border-[#ccc] rounded-md px-4 mb-[15px]'
-                        style={{ fontFamily: 'OpenSans-Regular', fontSize: RFValue(12) }}
-                        value={name}
-                        onChangeText={setName}
-                        placeholderTextColor={'#ccc'}
-                    />
+                    </ScrollView>
+                </KeyboardAvoidingView>
 
-                    <View className='flex-row justify-between'>
-                        <TouchableOpacity
-                            className='flex-1 bg-primary py-4 mx-2 rounded-3xl items-center '
-                            onPress={handleSave}
-                        >
-                            <CustomText
-                                fontFamily='OpenSans-Medium'
-                                style={{ color: 'white', fontWeight: 'bold' }}
-                            >
-                                Save
-                            </CustomText>
-                        </TouchableOpacity>
-                        <TouchableOpacity
-                            className='flex-1 bg-[#FF5A5F] py-4 mx-2 rounded-3xl items-center '
-                            onPress={onClose}
-                        >
-                            <CustomText
-                                fontFamily='OpenSans-Medium'
-                                style={{ color: 'white', fontWeight: 'bold' }}
-                            >
-                                Cancel
-                            </CustomText>
-                        </TouchableOpacity>
-                    </View>
-                </View>
+
             </View>
         </Modal>
     )
