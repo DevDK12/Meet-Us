fbb232b30612d06e3b06e5bdcfb99b44fc0e9e6f
devdk12 <devdk2208@gmail.com>
2025-02-25 02:18:17 +0530

    fix:fixed websocket url bug

--- src/services/config.ts
+++ src/services/config.ts
@@ -3,7 +3,7 @@ import { HOST } from '@env';
 // export const BASE_URL = `http://192.168.1.4:3000/api/v1`;
 // export const SOCKET_URL = `ws://192.168.1.4:3000`;
 export const BASE_URL = `https://${HOST}/api/v1`;
-export const SOCKET_URL = `ws://${HOST}`;
+export const SOCKET_URL = `wss://${HOST}`;
 
 
 export const CLOUD_NAME = process.env.CLOUD_NAME;
